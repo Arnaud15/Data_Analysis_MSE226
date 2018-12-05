1d156ece1537e692753d7fedc8c555fa0b33d2ce
LiyangSun <liyang.patricksun@gmail.com>
2018-12-04 16:43:48 -0800

    inference R

--- inference.R
+++ inference.R
@@ -2,3 +2,7 @@ library(tidyverse)
 
 df_train = read.csv("CleanedData/Features1_train.csv")
 df_test = read.csv("CleanedData/Features1_test.csv")
+df_train$X <- NULL
+df_test$X <- NULL
+
+model = lm(log_target ~ ., data=df_train)
